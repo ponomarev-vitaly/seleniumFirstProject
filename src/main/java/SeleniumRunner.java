41f5c8747bc3c22e569d338d1d4c33479fc04309
Vitaly Ponomarev <vp211082@gmail.com>
2023-06-13 02:08:04 +0300

    I added new dependency for WebDriver. I modified the code in the SeleniumRunner java class.

--- src/main/java/SeleniumRunner.java
+++ src/main/java/SeleniumRunner.java
@@ -1,8 +1,10 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumRunner {
     public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
     }
 }
