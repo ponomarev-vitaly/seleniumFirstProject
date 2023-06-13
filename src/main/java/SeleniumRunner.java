a2a7949d3da4fd1034944bc0af124e6f5547fb3d
ponomarev-vitaly <vp211082@gmail.com>
2023-06-13 20:06:44 +0300

    I added code for the primitive test of the github.com web-site. The test is working correctly.

--- src/main/java/SeleniumRunner.java
+++ src/main/java/SeleniumRunner.java
@@ -1,14 +1,30 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static io.github.bonigarcia.wdm.WebDriverManager.*;
 
 public class SeleniumRunner {
     public static void main(String[] args) {
+//        chromedriver().setup();
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://www.google.com");
+//        System.out.println(driver.getTitle());
+//        System.out.println(driver.getCurrentUrl());
+//        driver.quit();
+
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        System.out.println(driver.getTitle());
-        System.out.println(driver.getCurrentUrl());
+        driver.manage().window().maximize();
+        driver.get("https://github.com");
+
+        WebElement searchInput = driver.findElement(By.cssSelector("[name='q']"));
+        searchInput.sendKeys("Selenium");
+        searchInput.sendKeys(Keys.ENTER);
+
         driver.quit();
     }
 }
